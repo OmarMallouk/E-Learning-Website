85d220fe50e9376cb52a8b2091203c5b41ae4312
OmarMallouk <mr.mallouk.om@gmail.com>
2024-11-18 03:38:04 +0200

    added signup page

---
+++ frontend/src/components/navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "../styles/navbar.css"
+
+
+const Navbar = () => {
+
+    return(
+
+<nav className="navbar flex">
+      <div className="logo">
+        <img src="http://localhost/reactELearning/frontend/src/assets/elarn.jpg" alt="learnn" />
+      </div>
+      <h1>E-Learn</h1>
+    </nav>
+    );
+
+};
+
+export default Navbar;
---
+++ frontend/src/components/singup.jsx
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "../styles/signup.css"
+import { useNavigate } from 'react-router-dom';
+import Navbar from './navbar'
+
+const Signup =() =>{
+    const navigate = useNavigate();
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [role, setRole] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        try{
+            const response = await axios.post('http://localhost/reactELearning/backend/users.php',{
+                name: username,
+                password: password,
+                email: email,
+                role: role
+
+            });
+            if (response.data.success){
+                navigate('/');
+
+            }else{
+                setErrorMessage(response.data.error);
+            }
+        }catch (error){
+            console.error('Error during signup request: ', error);
+            setErrorMessage('An error occurred. Please try again.')
+        }
+    };
+
+    const goToLogin = () => {
+        navigate('/'); 
+    };
+    
+
+    return (
+        
+        <div className="signup-container">
+            <Navbar/>
+        <h2>Signup</h2>
+        <form id="signupForm" onSubmit={handleSubmit}>
+            <div className="form-group">
+                <label htmlFor="username">Username</label>
+                <input type="text" id="username" name="username" value={username}
+                onChange={(e) => setUsername(e.target.value)} required/>
+            </div>
+            <div className="form-group">
+                <label htmlFor="email">email</label>
+                <input type="text" id="email" name="email" value={email}
+                onChange={(e) => setEmail(e.target.value)} required/>
+            </div>
+            <div className="form-group">
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password" name="password" value={password} 
+                onChange={(e) => setPassword(e.target.value)} required/>
+            </div>
+
+            <div className="form-group">
+                    <label htmlFor="role">Role:</label>
+                    <select className="role"
+                        id="role"
+                        value={role}
+                        onChange={(e) => setRole(e.target.value)}
+                    >
+                        <option value="">Select type</option>
+                        <option value="income">Student</option>
+                        <option value="expense">Instructor</option>
+                    </select>
+                </div>
+            <button type="submit" className="btn">Signup</button>
+            {errorMessage && <div id="errorMessage" className="error-message">{errorMessage}</div>}
+        </form>
+        <button onClick={goToLogin}>login?</button>
+    </div>
+    
+    );
+};
+
+export default Signup
