069eddd1af551392a6d4bf2cb696cb0db680a4bf
OmarMallouk <mr.mallouk.om@gmail.com>
2024-11-21 05:22:28 +0200

    added some css

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../styles/navbar.css"
+import "../styles/adminDash.css"
 import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
@@ -21,7 +22,7 @@ const isLoggedIn = localStorage.getItem('token');
         <img src="http://localhost/reactELearning/frontend/src/assets/elarn.jpg" alt="learnn" />
       </div>
       <h1>E-Learn</h1>
-      {isLoggedIn && <li><button onClick={handleLogout}>Logout</button></li>}
+      {isLoggedIn && <li className="p1"><button onClick={handleLogout} className="b3">Logout</button></li>}
     </nav>
     );
 
