65e75e92ac9888ed7f92f83287e750fee78400cc
OmarMallouk <mr.mallouk.om@gmail.com>
2024-11-21 02:32:45 +0200

    added a fetch invitation function

--- frontend/src/components/user-dashboard.jsx
+++ frontend/src/components/user-dashboard.jsx
@@ -9,10 +9,36 @@ import Navbar from './navbar'
 
 const Student = () =>{
     const [courses, setCourses] = useState([]);
+    const [invitations, setInvitations] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
     const navigate = useNavigate();
+
+
+    useEffect(() => {
+        fetchInvitations();
+    }, []);
+
+
+    const fetchInvitations = async () => {
+        try {
+            const response = await axios.get("http://localhost/reactELearning/backend/get_invite.php", {
+                headers: {
+                    Authorization: `Bearer ${localStorage.token}`,
+                },
+            });
+            if (response.data.success) {
+                setInvitations(response.data.invitations);
+            } else {
+                alert("No invitations found.");
+            }
+        } catch (error) {
+            console.error("Error fetching invitations:", error);
+        }
+    };
+    
+
 
     useEffect(() => {
         // Fetch courses after the component mounts
@@ -38,6 +64,7 @@ const Student = () =>{
         };
     
         fetchCourses();
+        
       }, []); // runs once after mounts
     
       if (loading) {
@@ -47,7 +74,9 @@ const Student = () =>{
       if (error) {
         return <div>{error}</div>;
       }
-    
+
+
+ 
     return(
         <div className="User-dashboard">
             <Navbar/>
@@ -65,6 +94,24 @@ const Student = () =>{
           <li>No courses found.</li>
         )}
       </ul>
+
+
+      <div>
+            <h1>Your Course Invitations</h1>
+            {invitations.length === 0 ? (
+                <p>No invitations available.</p>
+            ) : (
+                <ul>
+                    {invitations.map(invite => (
+                        <li key={invite.invite_id}>
+                            Course: {invite.course_title}
+                            {/* <button onClick={() => acceptInvitation(invite.invite_id, invite.course_id)}>Accept</button> */}
+                            {/* <button onClick={() => declineInvitation(invite.invite_id)}>Decline</button> */}
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </div>
               
         </div>
 
